118ed915798d1681c110c0e5b4c6c2cccc48716e
Toby Jordan <24980389+tobyjordan@users.noreply.github.com>
2020-06-07 11:52:51 +0100

    started implementing stdlib

--- app.c
+++ app.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define DEBUG false
+#define DEBUG true
+#define CHARMODE true
 
 #define STACK_DEPTH 32
 #define MEMORY_SIZE 256
 
-static short PURGATORY;
+#define CHARSET "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.<>!?'-+/* \n"
 
-static short DATAPTR;
-static short INSTPTR;
+static short PURGATORY = 0;
+static short DATAPTR   = 0;
+static short INSTPTR   = 0;
 
 static short *DATASTACK;
 static short *CALLSTACK;
@@ -27,57 +29,53 @@ enum INSTRUCTION_SET
     pll,
     add,
     out,
-    jpz
+    zbr,
+    rtn
 };
 
-void load_prog(short program[], short length);
+static short STANDARDLIB[] = {
+    dpl,
+    psh, 255,
+    zbr,
+    out,
+    pop,
+    psh, 0,
+    psh, 244,
+    zbr,
+    rtn
+};
+
+void load_prog(short program[], short length, bool start);
+void print_debug(short cur_inst);
 
 int main(void)
 {
     short instr, op1, op2;
     short code[] =
 	{
-	    psh, 15,  // how many iterations
-	    psh, 1,
-	    psh, 1,
-
-	    out,      // print first two elements
-	    out,
-
-	    dpl,      // calculate next element
-	    pop,
-	    add,
-
-	    swp,      // loop
-	    psh, -1,
-	    add,
-	    dpl,
-	    psh, 28,  // halt if finished iterating
-	    jpz,
-	    swp,
-
-	    out,      // print next element
-
-	    pll,      // return to start
-	    swp,
 	    psh, 0,
-	    psh, 8,
-	    jpz,
-
+	    psh, 47,
+	    psh, 24,
+	    psh, 21,
+	    psh, 21,
+	    psh, 14,
+	    psh, 17,
+	    psh, 0,
+	    psh, 244,
+	    zbr,
 	    hlt
 	};
     
-    DATAPTR = INSTPTR = PURGATORY = 0;
     DATASTACK = malloc(STACK_DEPTH*sizeof(short));
     CALLSTACK = malloc(STACK_DEPTH*sizeof(short));
 
     MEMORY = malloc(MEMORY_SIZE*sizeof(short));
-    
-    load_prog(code, 29);
+
+    load_prog(STANDARDLIB, 12, false);
+    load_prog(code, 20, true);
     
     while ((instr = MEMORY[CALLSTACK[INSTPTR]++]))
     {
-	if (DEBUG) printf("%d: %d\n", CALLSTACK[INSTPTR]-1, instr);
 	switch (instr)
 	{
 	case (psh):
@@ -109,19 +107,35 @@ int main(void)
 	    break;
 
 	case (out):
-	    printf("%d\n", DATASTACK[DATAPTR]);
+	    if (!CHARMODE)
+	    {
+		printf("%d\n", DATASTACK[DATAPTR]);
+		break;
+	    }
+
+	    op1 = DATASTACK[DATAPTR];
+	    if (op1 > 0 && op1 < 65)
+		printf("%c", CHARSET[op1]);
+	    else
+		printf("<invalid character '%d'!>\n", op1);
+	    if (DEBUG) puts("");
 	    break;
 
-	case (jpz):
+	case (zbr):
 	    op1 = DATASTACK[DATAPTR--]; op2 = DATASTACK[DATAPTR--];
 	    if (!op2)
 		CALLSTACK[++INSTPTR] = op1;
 	    break;
+
+	case (rtn):
+	    INSTPTR--;
+	    break;
 	    
 	default:
-	    printf("encountered invalid opcode '%d'!\n", instr);
+	    printf("<invalid opcode '%d'!>\n", instr);
 	    break;
 	}
+	if (DEBUG) print_debug(instr);
     }
     
     free(DATASTACK);
@@ -130,8 +144,18 @@ int main(void)
     return 0;
 }
 
-void load_prog(short program[], short length)
+void load_prog(short program[], short length, bool start)
 {
+    int offset = start ? 0 : MEMORY_SIZE-length;
+
     for (int i = 0; i < length; i++)
-	MEMORY[i] = program[i];
+	MEMORY[i+offset] = program[i];
+}
+
+void print_debug(short cur_inst)
+{
+    printf("%d: %d [", CALLSTACK[INSTPTR]-1, cur_inst);
+    for (int i = 0; i < DATAPTR; i++)
+	printf(" %d", DATASTACK[i]);
+    printf(" ]\n");
 }
